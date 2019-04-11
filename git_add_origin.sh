c554f67b34331acb4f22eb6c30d8ccfd87e8dd25
ethan <konmyn@gmail.com>
2019-04-11 20:05:52 +0800

    add some more git default settings

---
+++ git_add_origin.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# my specific git settings
+
+git remote add origin git@github.com:Konmyn/cabinets.git
+git push -u origin master
--- git_init.sh
+++ git_init.sh
@@ -3,6 +3,24 @@
 
 git config --global user.name 'ethan'
 git config --global user.email 'konmyn@gmail.com'
+git config --global push.default simple
 
-echo "$(git config --global user.name)"
-echo "$(git config --global user.email)"
+# status 高亮模式为 auto
+git config --global color.status auto
+# branch 高亮模式为 auto
+git config --global color.branch auto
+# ui 高亮模式为 auto
+git config --global color.ui auto
+
+git config --global alias.lg "log --color --graph --pretty=format:'%C(bold red)%h%C(reset) - %C(bold green)(%cr)%C(bold blue)<%an>%C(reset) -%C(bold yellow)%d%C(reset) %s' --abbrev-commit"
+git config --global alias.st status
+
+# 解决中文文件显示问题
+git config --global core.quotepath false
+git config core.editor vim
+
+git config --global --list
+
+# you may also set below
+# git config --system
+# git config --system --list
