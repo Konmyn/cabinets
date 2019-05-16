f5e38ebecdcfaa8c6f107bf7bd64d740aa8d8837
ethan <konmyn@gmail.com>
2019-05-16 23:51:07 +0800

    change to default user

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -25,22 +25,23 @@ apt install open-vm-tools-desktop vim openssh-server curl zsh python3-pip tree h
 # disable login message for user
 touch $HOME/.hushlogin
 
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" exit
+
 # https://github.com/wting/autojump
-git clone git://github.com/wting/autojump.git
+sudo -u matrix git clone git://github.com/wting/autojump.git
 cd autojump
 ./install.py
 echo '[[ -s /home/matrix/.autojump/etc/profile.d/autojump.sh ]] && source /home/matrix/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
 echo 'autoload -U compinit && compinit -u' >> ~/.zshrc
 
 sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc && sed -i 's/alF/lh/g' ~/.bashrc
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo -u matrix git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # plugins=(zsh-autosuggestions)
 
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo -u matrix git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # plugins=( [plugins...] zsh-syntax-highlighting)
 sed -i 's/robbyrussell/ys/g' ~/.zshrc
 sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
